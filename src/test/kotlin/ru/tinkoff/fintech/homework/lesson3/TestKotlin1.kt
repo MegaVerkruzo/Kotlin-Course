0828de5528490509c10b47e0dcd4eeaec66154d1
Grunskii Alexey <robochat@mail.ru>
2022-03-11 00:04:59 +0300

    Complete 1 task

--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/TestKotlin1.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/TestKotlin1.kt
@@ -2,18 +2,26 @@ package ru.tinkoff.fintech.homework.lesson3
 
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
 import ru.tinkoff.fintech.homework.lesson1.*
 
 class TestKotlin1 : FeatureSpec(){
     init {
-        "Chicken" {
-            val chicken = Chicken(3, 3)
-            chicken.say() shouldBe "Cock-A-Doodle-Doo"
-        }
-        "Cat" {
-            val cat = Cat(4, 14)
-            cat.say() shouldBe "Meow"
+        feature("Бег кота") {
+            scenario("Немного бега") {
+                val cat = Cat(4, 14)
+                cat.run()
+                cat.energy shouldBeInRange 1..99
+            }
+            scenario("Бег до смерти") {
+                val cat = Cat(4, 14)
+                for (i in 1..10) {
+                    cat.run()
+                }
+                cat.energy shouldBe 0
+                cat.isAlive() shouldBe false
+            }
         }
     }
 }
