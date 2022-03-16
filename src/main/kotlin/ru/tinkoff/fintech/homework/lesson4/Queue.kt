d027729ee05a012a501973779dc9a0bb6dcf71ac
Grunskii Alexey <robochat@mail.ru>
2022-03-16 19:06:16 +0300

    ReWrite Queue

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/Queue.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/Queue.kt
@@ -4,36 +4,54 @@ import java.util.*
 import kotlin.math.max
 
 class Queue<T : Any> {
-    private class Node<T>(val value: T, var prev: Node<T>) {
+    private class Node<T>(val value: T, var prev: Node<T>?) {
     }
 
-    private lateinit var top: Node<T>
-    private lateinit var back: Node<T>
-    private var size: Int = 0
+    private var top: Node<T>? = null
+    private var back: Node<T>? = null
 
     fun element(): T {
-        if (size == 0) NoSuchElementException()
+        if (top == null) NoSuchElementException()
 
-        return top.value
+        return top!!.value
     }
 
     fun remove(): T {
-        if (size == 0) NoSuchElementException()
-        // Дописать
+        if (top == null) NoSuchElementException()
+        val result = top!!.value
+        top = top!!.prev
+        return result
     }
 
     fun peek(): T? {
-        if (size == 0) return null
+        if (top == null) return null
 
-        return top.value
+        return top!!.value
     }
 
     fun poll(): T? {
-        if (size == 0) NoSuchElementException()
-        // Дописать
+        if (top == null) null
+        val result = top!!.value
+        top = top!!.prev
+        return result
     }
 
-    fun offer(element: Any) {
-        if (ele)
+    fun offer(element: Any): Boolean {
+        if (element as? T == null) {
+            return false
+        }
+        if (top == null) {
+            top = Node(element, null)
+        } else {
+            val newNode = Node(element, null)
+            if (back == null) {
+                back = newNode
+                top!!.prev = back
+            } else {
+                back!!.prev = newNode
+                back = newNode
+            }
+        }
+        return true
     }
 }
