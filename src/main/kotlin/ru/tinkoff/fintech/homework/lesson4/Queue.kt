2f2237ec35b3036f2ae52fec1b8b357b2bf37c9b
Grunskii Alexey <robochat@mail.ru>
2022-03-16 16:41:32 +0300

    Save middle changes

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/Queue.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/Queue.kt
@@ -4,57 +4,36 @@ import java.util.*
 import kotlin.math.max
 
 class Queue<T : Any> {
-    private lateinit var elements: Array<T?>
-    private var head = 0
-    private var tail = 0
-    var size = 0
-        private set
-
-    fun offer(element: T): Boolean {
-        ensureCapacity()
-        if (tail == head && size > 0) {
-            System.arraycopy(elements, 0, elements, elements.size / 2, tail)
-            tail += elements.size / 2
-        }
-        size += 1
-        elements[tail] = element
-        tail = nextElement(tail)
-        return true
+    private class Node<T>(val value: T, var prev: Node<T>) {
     }
 
-    fun element(): T? {
+    private lateinit var top: Node<T>
+    private lateinit var back: Node<T>
+    private var size: Int = 0
+
+    fun element(): T {
         if (size == 0) NoSuchElementException()
-        return elements[head]
+
+        return top.value
+    }
+
+    fun remove(): T {
+        if (size == 0) NoSuchElementException()
+        // Дописать
     }
 
     fun peek(): T? {
-        if (size == 0) NoSuchElementException()
-        return elements[head]
-    }
+        if (size == 0) return null
 
-    fun remove(): T? {
-        if (size == 0) NoSuchElementException()
-        val result: T? = elements[head]
-        size -= 1
-        head = nextElement(head)
-        return result
+        return top.value
     }
 
     fun poll(): T? {
         if (size == 0) NoSuchElementException()
-        val result: T? = elements[head]
-        size -= 1
-        head = nextElement(head)
-        return result
+        // Дописать
     }
 
-    private fun ensureCapacity() {
-        if (elements.size == size) {
-            elements = elements.copyOf(max(size * 2, 1))
-        }
-    }
-
-    private fun nextElement(index: Int): Int {
-        return (index + 1) % elements.size
+    fun offer(element: Any) {
+        if (ele)
     }
 }
