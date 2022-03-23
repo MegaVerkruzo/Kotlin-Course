b07211ee85d30abce51ca4d3e5794a8a4b516e7d
Grunskii Alexey <robochat@mail.ru>
2022-03-23 22:20:07 +0300

    Change to shouldContainAll

--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson5/ServiceTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson5/ServiceTest.kt
@@ -1,6 +1,8 @@
 package ru.tinkoff.fintech.homework.lesson5
 
 import io.kotest.core.spec.style.FeatureSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 
 class ServiceTest : FeatureSpec() {
@@ -9,37 +11,85 @@ class ServiceTest : FeatureSpec() {
     init {
         feature("Проверка сервиса") {
             scenario("Проверка перевода на Американский рынок") {
-                val translatedDescription = service.onEnglish(cars)
+                val result = listOf(
+                    Car(model = "TTS",
+                        brand = "AUDI",
+                        body = "SPORTSCAR",
+                        cost = 27400.0,
+                        gasolineConsumption = 6.4),
+                    Car(
+                        model = "Vesta",
+                        brand = "LADA",
+                        body = "FOURDOORSEDAN",
+                        cost = 10380.0,
+                        gasolineConsumption = 6.3
+                    ),
+                    Car(model = "M4",
+                        brand = "BMW",
+                        body = "SPORTSCAR",
+                        cost = 78000.0,
+                        gasolineConsumption = 8.3),
+                    Car(
+                        model = "911 Carrera",
+                        brand = "PORSCHE",
+                        body = "SPORTSCAR",
+                        cost = 87900.0,
+                        gasolineConsumption = 9.6
+                    ),
+                    Car(
+                        model = "730Ld xDrive",
+                        brand = "BMW",
+                        body = "LIMOUSINE",
+                        cost = 101145.0,
+                        gasolineConsumption = 3.0
+                    ),
+                    Car(
+                        model = "X6 M50d M Special",
+                        brand = "BMW",
+                        body = "HARDTOP",
+                        cost = 179990.0,
+                        gasolineConsumption = 3.0
+                    )
+                )
 
-                print(translatedDescription)
-                translatedDescription[0] shouldBe Car(model="Vesta", brand="LADA", body="FOURDOORSEDAN", cost=10380.0, gasolineConsumption=6.3)
-                translatedDescription[1] shouldBe Car(model="TTS", brand="AUDI", body="SPORTSCAR", cost=27400.0, gasolineConsumption=6.4)
-                translatedDescription[2] shouldBe Car(model="M4", brand="BMW", body="SPORTSCAR", cost=78000.0, gasolineConsumption=8.3)
-                translatedDescription[3] shouldBe Car(model="911 Carrera", brand="PORSCHE", body="SPORTSCAR", cost=87900.0, gasolineConsumption=9.6)
-                translatedDescription[4] shouldBe Car(model="730Ld xDrive", brand="BMW", body="LIMOUSINE", cost=101145.0, gasolineConsumption=3.0)
-                translatedDescription[5] shouldBe Car(model="X6 M50d M Special", brand="BMW", body="HARDTOP", cost=179990.0, gasolineConsumption=3.0)
+                service.onEnglish(cars) shouldContainAll result
             }
 
             scenario("Проверка группировки по брэнду") {
                 val groupedCars = service.groupByBrand(cars)
 
-                println(groupedCars)
             }
 
             scenario("Проверка группировки по кузову") {
                 val groupedCars = service.groupByBrand(cars)
 
-                println(groupedCars)
             }
 
             scenario("Топ 3 дешёвых машины PORSCHE и BMW") {
-                val groupOfCars = service.suitableList(cars) { car: Car -> car.brand == "БМВ" || car.brand == "ПОРШ" }
+                val result = listOf(
+                    Car(
+                        model = "911 Carrera",
+                        brand = "PORSCHE",
+                        body = "SPORTSCAR",
+                        cost = 87900.0,
+                        gasolineConsumption = 9.6
+                    ),
+                    Car(
+                        model = "730Ld xDrive",
+                        brand = "BMW",
+                        body = "LIMOUSINE",
+                        cost = 101145.0,
+                        gasolineConsumption = 3.0
+                    ),
+                    Car(model = "M4",
+                        brand = "BMW",
+                        body = "SPORTSCAR",
+                        cost = 78000.0,
+                        gasolineConsumption = 8.3
+                    )
+                )
 
-                println(groupOfCars)
-                groupOfCars.size shouldBe 3
-                groupOfCars[0] shouldBe Car(model="M4", brand="BMW", body="SPORTSCAR", cost=78000.0, gasolineConsumption=8.3)
-                groupOfCars[1] shouldBe Car(model="911 Carrera", brand="PORSCHE", body="SPORTSCAR", cost=87900.0, gasolineConsumption=9.6)
-                groupOfCars[2] shouldBe Car(model="730Ld xDrive", brand="BMW", body="LIMOUSINE", cost=101145.0, gasolineConsumption=3.0)
+                service.suitableList(cars) { car: Car -> car.brand == "БМВ" || car.brand == "ПОРШ" } shouldContainAll result
             }
         }
     }
@@ -48,7 +98,7 @@ class ServiceTest : FeatureSpec() {
 val cars = listOf(
     Car("730лд иксДрайв", "БМВ", "ЛИМУЗИН", 10114500.0, 3.0),
     Car("икс6 М50д М спешиал", "БМВ", "СЕДАН", 17999000.0, 3.0),
-    Car("911 каррера", "ПОРШ", "СПОРТКАР", 8790000.0,9.6),
+    Car("911 каррера", "ПОРШ", "СПОРТКАР", 8790000.0, 9.6),
     Car("Веста", "ЛАДА", "ЧЕТЫРЁХДВЕРНОЙСЕДАН", 1038000.0, 6.3),
     Car("М4", "БМВ", "СПОРТКАР", 7800000.0, 8.3),
     Car("ТТС", "АУДИ", "СПОРТКАР", 2740000.0, 6.4)
