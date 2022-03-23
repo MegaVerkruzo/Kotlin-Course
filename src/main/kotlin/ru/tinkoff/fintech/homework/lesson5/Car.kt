8f7c179a6c40c4bbdf00dc2199e326512ce96f4b
Grunskii Alexey <robochat@mail.ru>
2022-03-23 22:07:59 +0300

    Format code

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson5/Car.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson5/Car.kt
@@ -1,4 +1,10 @@
 package ru.tinkoff.fintech.homework.lesson5
 
-data class Car(val model: String, val brand: String, val body: String, val cost: Double, val gasolineConsumption: Double)
+data class Car(
+    val model: String,
+    val brand: String,
+    val body: String,
+    val cost: Double,
+    val gasolineConsumption: Double
+)
 
