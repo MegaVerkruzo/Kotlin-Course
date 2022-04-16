fd1cad493cec070b0d91887d933f80186b2dbd0a
Grunskii Alexey <robochat@mail.ru>
2022-04-16 14:35:34 +0300

    Add Bean jdbcTemplate

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,17 +16,22 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2:2.1.210")
+    implementation("org.liquibase:liquibase-core:4.9.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.2.2")
     testImplementation("io.mockk:mockk:1.12.3")
 }
 
--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/configuration/ServiceConfiguration.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/configuration/ServiceConfiguration.kt
@@ -3,19 +3,24 @@ package ru.tinkoff.fintech.homework.lesson6.configuration
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.client.RestTemplate
 import ru.tinkoff.fintech.homework.lesson6.common.model.Cake
 import ru.tinkoff.fintech.homework.lesson6.common.model.Order
 import java.time.Duration
+import javax.sql.DataSource
 
 @Configuration
-class ServiceConfiguration {
+abstract class ServiceConfiguration {
 
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder
         .setConnectTimeout(Duration.ofSeconds(CONNECT_TIMEOUT_IN_SECONDS))
         .setReadTimeout(Duration.ofSeconds(READ_TIMEOUT_IN_SECONDS))
         .build()
+
+    @Bean
+    fun jdbcTemplate(dataSource: DataSource): JdbcTemplate = JdbcTemplate(dataSource)
 }
 
 private const val CONNECT_TIMEOUT_IN_SECONDS = 30L
